7680d28caa48c009ee784c006f67d1dcd191854c
Swastik Bhattacharjee <159615363+SwastikBhattacharjee@users.noreply.github.com>
2024-02-17 11:27:28 +0530

    Add files via upload

---
+++ Leetcode4.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    def findMedianSortedArrays(self, nums1, nums2):
+        """
+        :type nums1: List[int]
+        :type nums2: List[int]
+        :rtype: float
+        """
+        nums = sorted(nums1 + nums2)
+
+        if len(nums) % 2 == 0:
+            return (nums[len(nums) // 2] + nums[(len(nums) // 2) - 1]) / 2
+        else:
+            return nums[len(nums) // 2]
+        
+        
+print(Solution().findMedianSortedArrays([1,2], [3,4]))
